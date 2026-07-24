4a604898a4d7dff583e81b63212f6b56f163a1a7
Matthew Hamilton <matthew.hamilton2@unibo.it>
2026-07-24 21:50:04 +0100

    start convolution test

--- AuroraTests/main.cpp
+++ AuroraTests/main.cpp
@@ -10,86 +10,57 @@ int main()
     
     const size_t blockSize = 1024;
     size_t numSamples = ssweep.GetSamplerate() * 10;
-    auto data = std::make_unique<float[]>(blockSize);
+    auto filter = std::make_unique<float[]>(blockSize);
+    auto data   = std::make_unique<float[]>(blockSize);
     size_t i = 0;
     
     ssweep.Generate();
     
-    while (i < numSamples)
-    {
-        const size_t currentBlock = std::min(blockSize, numSamples - i);
-
-        ssweep.FillBlock(data.get() + i, currentBlock, i, 0);
-
-        i += currentBlock;
-    }
+//    while (i < numSamples)
+//    {
+//        const size_t currentBlock = std::min(blockSize, numSamples - i);
+//        
+//        ssweep.FillBlock(data.get() + i,   currentBlock, i,   0);
+//        
+//        i += currentBlock;
+//    }
+//    i=0;
+//    while (i < numSamples)
+//    {
+//        const size_t currentBlock = std::min(blockSize, numSamples - i);
+//        
+//        ssweep.FillBlock(filter.get() + i, currentBlock, i, 1);
+//        
+//        i += currentBlock;
+//    }
+    //    memcpy(data.get(), ssweep.GetBuffer(0).CSamples(), numSamples);
+    //    memcpy(filter.get(), ssweep.GetBuffer(1).CSamples(), numSamples);
     
-    writeToWav(data.get(), numSamples, "test.wav");
+    i=0;
+    ssweep.FillBlock(data.get() + i,   numSamples, 0, 0);
+    ssweep.FillBlock(filter.get() + i, numSamples, 0, 1);
+    writeToWav(data.get(),   numSamples, "sweep-audio.wav");
+    writeToWav(filter.get(), numSamples, "sweep-inver.wav");
+    
+    writeToWav(const_cast<float*>(ssweep.GetBuffer(0).CSamples()), numSamples, "sweep-normal.wav");
+    writeToWav(const_cast<float*>(ssweep.GetBuffer(1).CSamples()), numSamples, "sweep-invers.wav");
     
     Aurora::ConvolverController convolver{};
     // Init()
     convolver.Reset();
     convolver.SetSamplerate(ssweep.GetSamplerate()); // ???
-    // LoadTrack Data
-//    SetFilterMatrixDimensions
-//    DoConvolution OR DoMatrixConvolution
+    convolver.CheckSamplerate(ssweep.GetSamplerate());
+//    convolver.ResizeFilterTrack(0,numSamples);
+//    convolver.ResizeInputTrack(0, numSamples);
     
-//    bool Aurora::ConvolverEffect::LoadTrackData(WaveTrack *wt, int idx, bool is_ir)
-//    {
-//        Aurora::SampleCount start;
-//        Aurora::SampleCount end;
-//
-//        //Get start and end times from track
-//        double trackStart = wt->GetStartTime();
-//        double trackEnd   = wt->GetEndTime();
-//
-//        //Set the current bounds to whichever left marker is
-//        //greater and whichever right marker is less:
-//        double t0 = mT0 < trackStart? trackStart: mT0;
-//        double t1 = mT1 > trackEnd  ? trackEnd  : mT1;
-//
-//        // Process only if the right marker is to the right of the left marker
-//        if (t1 > t0)
-//        {
-//            //Checks the track rate, samples, minimum, maximum and name
-//            if (! CheckSamplerate(wt->GetRate()))
-//            {
-//                return false;
-//            }
-//
-//            //Transform the marker timepoints to samples
-//            start = wt->TimeToLongSamples(t0).as_size_t();
-//            end   = wt->TimeToLongSamples(t1).as_size_t();
-//
-//            if(is_ir)
-//            {
-//                if (ResizeFilterTrack(idx, end - start))
-//                {
-//                    Aurora::Vector<Aurora::Sample>& f = GetFilters()[idx];
-//                    
-//                    wt->Get((samplePtr)f.Samples(),
-//                            floatSample,
-//                            sampleCount(start),
-//                            f.Length());
-//                }
-//            }
-//            else
-//            {
-//                if (ResizeInputTrack(idx, end - start))
-//                {
-//                    Aurora::Vector<Aurora::Sample>& in = GetInputTrack(idx);
-//                    wt->Get((samplePtr)in.Samples(),
-//                            floatSample,
-//                            sampleCount(start),
-//                            in.Length());
-//                }
-//            }
-//        }
-//        return true;
-//    }
-//    
+//    auto& filters = convolver.GetFilters();
     
-    
+    // put data in filters
+//    filters[0].Samples();
+
+    // put data in input
+//    auto& input = convolver.GetInputTrack(0);
+ 
     std::cout << "convolver.GetGain():" << convolver.GetGain() << '\n';
     
     return 0;
