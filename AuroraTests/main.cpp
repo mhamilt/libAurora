0b72e14f5cf2bc6cbd5096a9792cfda498455cfb
Matthew Hamilton <matthew.hamilton2@unibo.it>
2026-07-25 09:33:25 +0100

    working convolver example

--- AuroraTests/main.cpp
+++ AuroraTests/main.cpp
@@ -7,37 +7,40 @@
 int main()
 {
     Aurora::SineSweepGenerator ssweep{};
-    
-    const size_t blockSize = 1024;
     size_t numSamples = ssweep.GetSamplerate() * 10;
-    auto filter  = std::make_unique<float[]> (numSamples);
+    auto filter  = std::make_unique<float[]>(numSamples);
     auto audio   = std::make_unique<float[]>(numSamples);
-    size_t i = 0;
+    
     ssweep.Generate();
     
-    ssweep.FillBlock(audio.get()  + i,   numSamples, 0, 0);
-    ssweep.FillBlock(filter.get() + i,  numSamples, 0, 1);
+    ssweep.FillBlock(audio.get() ,  numSamples, 0, 0); // Sweep  == Channel_1
+    ssweep.FillBlock(filter.get(),  numSamples, 0, 1); // Filter == Channel_2
+    
+    Aurora::ConvolverController convolver{};
+    convolver.Reset();
+    convolver.SetSamplerate(ssweep.GetSamplerate()); // ???
+    convolver.CheckSamplerate(ssweep.GetSamplerate());
+    convolver.SetFilterMatrixDimensions(1,1);
+    convolver.ResizeFilterTrack(0,numSamples);
+    convolver.ResizeInputTrack(0, numSamples);
+    
+    auto& convolutionFilters = convolver.GetFilters();
+    auto& input = convolver.GetInputTrack(0);
+    
+    std::copy_n(filter.get(), numSamples, convolutionFilters[0].Samples());
+    std::copy_n(audio.get(),  numSamples, input.Samples());
+
+    convolver.DoConvolution();
+    
+    auto& conv = convolver.GetOutputTrack(0);
     
     writeToWav(audio.get(),  (uint32_t)numSamples, "sweep-audio.wav");
     writeToWav(filter.get(), (uint32_t)numSamples, "sweep-inver.wav");
+    writeToWav(conv.Samples(),  (uint32_t)numSamples, "sweep-convo.wav");
     
-    Aurora::ConvolverController convolver{};
-    // Init()
-    //    convolver.Reset();
-    //    convolver.SetSamplerate(ssweep.GetSamplerate()); // ???
-    //    convolver.CheckSamplerate(ssweep.GetSamplerate());
-    //    convolver.ResizeFilterTrack(0,numSamples);
-    //    convolver.ResizeInputTrack(0, numSamples);
-    
-    //    auto& filters = convolver.GetFilters();
-    
-    // put data in filters
-    //    filters[0].Samples();
-    
-    // put data in input
-    //    auto& input = convolver.GetInputTrack(0);
-    
-    std::cout << "convolver.GetGain():" << convolver.GetGain() << '\n';
+    for (int i = numSamples-10; i < numSamples; i++) {
+        std::cout << conv.Samples()[i] << '\n';
+    }
     
     return 0;
 }
